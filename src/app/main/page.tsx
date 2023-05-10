a1cc5895ebdafd85d5af0957af0f67c862757f86
imedra <marcbernardino2005@gmail.com>
2023-05-10 00:04:07 -0500

    developed frontend

---
+++ src/app/main/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <main className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+        <h1 className="text-3xl font-semibold pb-2">Awaiting Spotify Auth</h1>
+        </div>
+      </main>
+  )
+}
